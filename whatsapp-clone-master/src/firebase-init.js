90258861be4f4c0b11bafd0a9255f16c90528948
310624205015-eng <236999271+310624205015-eng@users.noreply.github.com>
2026-01-20 12:50:26 +0530

    Clear Firebase configuration values
    Updated Firebase configuration with empty values.

--- whatsapp-clone-master/src/firebase-init.js
+++ whatsapp-clone-master/src/firebase-init.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBoqrkjjOaAly5MD2Cd_hAldGLz6isMThM",
-    authDomain: "whatsapp-clone-2-372312.firebaseapp.com",
-    projectId: "whatsapp-clone-2-372312",
-    storageBucket: "whatsapp-clone-2-372312.appspot.com",
-    messagingSenderId: "225921394239",
-    appId: "1:225921394239:web:66e9a0b9cd976ec204ba8a"
+    apiKey: "",
+    authDomain: "",
+    projectId: "0",
+    storageBucket: "0",
+    messagingSenderId: "0",
+    appId: "0"
   };
   
   // Initialize Firebase
